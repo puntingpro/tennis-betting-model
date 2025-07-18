48a21e268be5767cbbf64801b6b5efe4d13c9018
iincincible <luca.panozzo13@gmail.com>
2025-07-18 18:36:39 +1000

    CI: Fix test discovery and package setup

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="puntingpro",
     version="0.1.0",
-    packages=find_packages(),
+    # FIX: Tell setuptools that packages are under the 'src' directory
+    package_dir={"": "src"},
+    # FIX: Find packages within the 'src' directory
+    packages=find_packages(where="src"),
 )
