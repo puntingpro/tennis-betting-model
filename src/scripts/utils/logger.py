ab0be00fe55a2a1f045b7cdd2a554a5fb6c772ee
iincincible <luca.panozzo13@gmail.com>
2025-07-18 14:12:22 +1000

    Fix: Format logger.py to pass Black linting

--- src/scripts/utils/logger.py
+++ src/scripts/utils/logger.py
@@ -1,6 +1,8 @@
 import logging
 import sys
 
+from pythonjsonlogger import jsonlogger
+
 
 def setup_logging(level: str = "INFO", json_logs: bool = False) -> None:
     """Configure the root logger for the application."""
